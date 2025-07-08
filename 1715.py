81c4bd3889e2f361d7df51a048c4cc41d51d12ce
DESKTOP-HA97GMI\SSAFY <jwyang34@gmail.com>
2025-07-08 09:38:30 +0900

    1715-1

---
+++ 1715.py
@@ -0,0 +1,16 @@
+# 틀렸음
+# N = int(input())
+
+# arr = []
+# sum = 0
+
+# for i in range(N):
+#     card = int(input())
+#     arr.append(card)
+    
+# arr.sort()
+# for j in range(1, N):
+#     arr[j] = arr[j - 1] + arr[j]
+#     sum += arr[j]
+        
+# print(sum)
