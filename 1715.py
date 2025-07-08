465c175db4a7942daa3c70386e17c0225442a957
DESKTOP-HA97GMI\SSAFY <jwyang34@gmail.com>
2025-07-08 09:54:32 +0900

    1715-2

--- 1715.py
+++ 1715.py
@@ -1,16 +1,27 @@
-# 틀렸음
-# N = int(input())
+'''
+오답노트: 
+원래는 정렬해서 앞에서부터 더함,
+그러나 파일을 합칠 때마다
+매번 새롭게 가장 작은 수 2개를 택해서 더해야 함
+'''
 
-# arr = []
-# sum = 0
+# 포인트: 힙큐를 사용해서 가장 작은 수를 더하기
 
-# for i in range(N):
-#     card = int(input())
-#     arr.append(card)
-    
-# arr.sort()
-# for j in range(1, N):
-#     arr[j] = arr[j - 1] + arr[j]
-#     sum += arr[j]
-        
-# print(sum)
+import heapq
+
+N = int(input())
+heap = []
+
+for _ in range(N):
+    heapq.heappush(heap, int(input()))
+
+total = 0
+
+while len(heap) > 1:
+    a = heapq.heappop(heap)
+    b = heapq.heappop(heap)
+    temp = a + b
+    total += temp
+    heapq.heappush(heap, temp)
+
+print(total)
