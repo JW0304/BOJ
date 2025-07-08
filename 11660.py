192269598f94ab2e1b2e707b4e81813d4fc08936
DESKTOP-HA97GMI\SSAFY <jwyang34@gmail.com>
2025-07-08 14:32:07 +0900

    11660-2

--- 11660.py
+++ 11660.py
@@ -1,37 +1,38 @@
-# 브루트 포스, 시간 초과
+# 최적화용 input
+import sys
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
-matrix = []
-row = []
+matrix = [list(map(int, input().split())) for _ in range(N)]
 
-for _ in range(N):
-    row = list(map(int, input().split()))
-    matrix.append(row)
-    
+prefix = [[0] * (N + 1) for _ in range(N + 1)]
+
+# 누적합 구하기 (전체 매트릭스)
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        prefix[i][j] = (matrix[i - 1][j - 1]
+                        + prefix[i - 1][j]
+                        + prefix[i][j - 1]
+                        - prefix[i - 1][j - 1])
+
+# 구간합 구하기 ((x1, y1) ... (x2, y2))
 for _ in range(M):
     x1, y1, x2, y2 = map(int, input().split())
-    total = 0
     
-    r1, c1, r2, c2 = x1-1, y1-1, x2-1, y2-1
+    result = (prefix[x2][y2]
+             - prefix[x1 -1][y2]
+             - prefix[x2][y1 - 1]
+             + prefix[x1 - 1][y1 - 1])
     
-    for r in range(N):
-        for c in range(N):
-            if r1 <= r <= r2 and c1 <= c <= c2:
-                    total += matrix[r][c]
-                
-    print(total)
+    print(result)
     
 '''
-행, 열 r, c
-row 초기화
-matrix는 2차원
+브루트포스 시간초과
 
-(x1, y1), (x2, y2)
-x1부터 x2
-y1부터 y2
-
-for문에서 범위 지정
-x1 <= x <= x2
-y1 <= y <= y2
+시간초과 해결:
+- readline
+- 2차원 누적합
+누적합 = 해당 행렬값 + 위쪽 누적합 + 왼쪽 누적합 - 겹친 부분 제외
+구간합 = (x2, y2) 누적합 - 위쪽 누적합 - 왼쪽 누적합 + 겹친 부분 복구
 '''
